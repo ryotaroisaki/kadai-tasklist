957addae9c7b6d7be012453d2fb6fd9e46b5a390
ryotaro.isaki <mdask8ryotaro@gmail.com>
2019-04-01 06:55:54 +0000

    kadai task-list resubmission

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
 
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content, :title)
+    params.require(:task).permit(:content, :status)
   end
   
   def set_task
