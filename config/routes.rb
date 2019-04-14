955315de49c0afc888877041e761ac9f3c6db982
ryotaro.isaki <mdask8ryotaro@gmail.com>
2019-04-14 03:21:40 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  root to: 'toppages#index'
+  
+  resources :users, only: [:index, :show, :new, :create]
+  
+
 
   resources :tasks
 
