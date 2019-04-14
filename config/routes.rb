e3c2c437d0cfe07ccf93c4e9ca5ce4966f6a7ecd
ryotaro.isaki <mdask8ryotaro@gmail.com>
2019-04-14 12:52:55 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,28 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
   root to: 'toppages#index'
   
+  get "signup", to: "users#new"
   resources :users, only: [:index, :show, :new, :create]
   
 
-
   resources :tasks
 
+get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 end
